4f8762398d2125d5fa154ae1fe3c706c705fd39d
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-02-18 11:48:55 -0300

    first commit

---
+++ app/src/main/java/br/edu/infnet/tasksapp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package br.edu.infnet.tasksapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.infnet.tasksapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private val bindind by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(bindind.root)
+    }
+}
