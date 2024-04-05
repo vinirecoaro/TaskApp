797a6e66e4bd5450877923bf1ee840460ae34c93
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-04-04 21:07:48 -0300

    ResetPasswordActivity - Added email fragment to verify if email is valid

--- app/src/main/java/br/edu/infnet/tasksapp/presentation/activities/reset_password/ResetPasswordActivity.kt
+++ app/src/main/java/br/edu/infnet/tasksapp/presentation/activities/reset_password/ResetPasswordActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
@@ -13,12 +15,15 @@ import br.edu.infnet.tasksapp.databinding.ActivityResetPasswordBinding
 import br.edu.infnet.tasksapp.presentation.activities.login.LoginActivity
 import br.edu.infnet.tasksapp.presentation.fragments.button.ButtonFragment
 import br.edu.infnet.tasksapp.presentation.fragments.button.OnButtonClickListener
+import br.edu.infnet.tasksapp.presentation.fragments.email_edit_text.EmailEditTextFragment
 import com.google.android.material.snackbar.Snackbar
 
 class ResetPasswordActivity : AppCompatActivity(), OnButtonClickListener {
 
     private val binding by lazy { ActivityResetPasswordBinding.inflate(layoutInflater)}
     private val viewModel by viewModels<ResetPasswordViewModel>()
+    private val buttonFragment = ButtonFragment()
+    private val emailFragment = EmailEditTextFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,13 +39,14 @@ class ResetPasswordActivity : AppCompatActivity(), OnButtonClickListener {
         if (savedInstanceState == null) {
 
             val bundle = bundleOf(getString(R.string.button_text_key) to "Enviar")
-            val fragment = ButtonFragment()
-            fragment.arguments = bundle
+
+            buttonFragment.arguments = bundle
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
-                add(R.id.frag_button_send, fragment)
+                add(R.id.frag_button_send, buttonFragment)
+                add(R.id.frag_et_email_reset_password, emailFragment)
             }
-            fragment.listener = this
+            buttonFragment.listener = this
 
         }
 
@@ -56,7 +62,7 @@ class ResetPasswordActivity : AppCompatActivity(), OnButtonClickListener {
         }
 
         viewModel.onResetPasswordFail = {
-            Snackbar.make(binding.btSend,getString(R.string.fail_redefine_password_message), Snackbar.LENGTH_LONG).show()
+            Snackbar.make(binding.fragButtonSend,getString(R.string.fail_redefine_password_message), Snackbar.LENGTH_LONG).show()
         }
 
         binding.resetPasswordToolbar.setNavigationOnClickListener {
@@ -67,7 +73,7 @@ class ResetPasswordActivity : AppCompatActivity(), OnButtonClickListener {
     private fun verifyFields(vararg text: EditText) : Boolean{
         for (i in text){
             if (i.text.toString() == "" || i == null){
-                Snackbar.make(binding.btSend,"${getString(R.string.fill_field)} ${i.hint}", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(binding.fragButtonSend,"${getString(R.string.fill_field)} ${i.hint}", Snackbar.LENGTH_LONG).show()
                 return false
             }
         }
@@ -75,8 +81,14 @@ class ResetPasswordActivity : AppCompatActivity(), OnButtonClickListener {
     }
 
     override fun onButtonClicked() {
-        if(verifyFields(binding.etEmail)){
-            viewModel.resetPassword(binding.etEmail.text.toString())
+        val editTextEmailFragment = emailFragment.requireView().findViewById<EditText>(R.id.et_email_frag)
+        val textViewEmailFragmet = emailFragment.requireView().findViewById<TextView>(R.id.tv_email_validity).text.toString()
+        if(verifyFields(editTextEmailFragment)){
+            if(textViewEmailFragmet == getString(R.string.valid_email)){
+                viewModel.resetPassword(editTextEmailFragment.text.toString())
+            }else{
+                Toast.makeText(this, getString(R.string.type_valid_email),Toast.LENGTH_LONG).show()
+            }
         }
     }
 
